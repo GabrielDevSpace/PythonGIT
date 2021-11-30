f8224a186b72b8728c275e286fabd63f294dd5fa
Gabriel Pires <64210900+GabrielDevSpace@users.noreply.github.com>
2021-11-30 10:34:57 -0300

    Estudos Python part2

---
+++ EstudosPythonParte2/JogoForca.py
@@ -0,0 +1,10 @@
+import random
+
+
+print("**************************************************")
+print("Jogo de forca para aplicar o aprendizado de python")
+print("**************************************************")
+
+palavra_secreta = ['banana','carro','legumes','submarino','aviador','sorte']
+identificador = random.randint(0,5)
+print(palavra_secreta[identificador])
