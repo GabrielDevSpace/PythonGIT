109a9da53127d1754119a7197859bd191cd72151
Gabriel Pires <gabrielpirestj@hotmail.com>
2021-12-08 14:40:53 -0300

    Edição Basica

--- EstudosPythonParte2/JogoForca.py
+++ EstudosPythonParte2/JogoForca.py
@@ -1,10 +1,17 @@
 import random
-
-
 print("**************************************************")
 print("Jogo de forca para aplicar o aprendizado de python")
 print("**************************************************")
 
-palavra_secreta = ['banana','carro','legumes','submarino','aviador','sorte']
-identificador = random.randint(0,5)
-print(palavra_secreta[identificador])
+palavras_secreta = ['banana','carro','legumes','submarino','aviador','sorte']
+quantidade_palavras_secreta = len(palavras_secreta) - 1
+identificador = random.randint(0,quantidade_palavras_secreta)
+palavra_escolhida = (palavras_secreta[identificador])
+print(palavra_escolhida)
+
+falhou = False
+acertou = False
+
+while (not falhou and not acertou):
+    chute = input("Digite uma LETRA: ")
+    print("Jogando...")
